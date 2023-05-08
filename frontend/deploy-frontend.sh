f91ceb740eb414ed851e416c78497611963bf810
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2023-05-08 09:08:20 +0000

    Update deploy-frontend.sh

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 set -xe
 sudo usermod -aG docker $USER
-sudo setcap cap_net_bind_service=+ep /usr/bin/node
 docker network create -d bridge momo_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull ${CI_REGISTRY_IMAGE}/momo-frontend:latest
@@ -11,5 +10,5 @@ docker run -d --name momofront \
     --network=momo_network \
     --restart always \
     --pull always \
-    -p 80:80 \
+    -p 8080:8080 \
     ${CI_REGISTRY_IMAGE}/momo-frontend:latest 
