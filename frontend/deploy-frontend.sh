28cdf8072802447a403ab3d9e6d7a5d0b954ed5d
Валерия Шелгунова <valerie.shelgunova@yandex.ru>
2023-05-08 09:07:13 +0000

    Update deploy-frontend.sh

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -4,7 +4,7 @@ sudo usermod -aG docker $USER
 sudo setcap cap_net_bind_service=+ep /usr/bin/node
 docker network create -d bridge momo_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker pull ${CI_REGISTRY_IMAGE}/sausage-frontend:latest
+docker pull ${CI_REGISTRY_IMAGE}/momo-frontend:latest
 docker stop momofront || true
 docker rm momofront || true
 docker run -d --name momofront \
